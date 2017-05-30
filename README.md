d28dde9eff40bce69631137ffc2e83dc9add183a
Berlin Sky <pedroyoung8989@gmail.com>
2017-05-29 22:37:26 -0700

    save and delete

--- README.md
+++ README.md
@@ -11,11 +11,25 @@
 ### Steps: 
 1. Add "favicon.png" to the images folder
 2. Update index.pug to show the favicon.
+3. Install runtime libraries
+```
+npm i body-parser -S
+npm i lodash -S
+```
+4. Add "getUserFilePath" method
+5. Add "getUser" method
+6. Add "saveUser" method
+7. Add references to "lodash" and "body-parser"
+8. Create a scripts folder
+9. Add "user.js" to the scripts folder
+10. Create a styltes folder
+11. Add user.csss to the styles folder
+12. Update the root "/" route
+13. Add get user name route
+13. Add put user name route
+13. Add delete user name route
+14. Create a users folder under data
+14. Add individual user.json files to the users folder
 
-
-1. Update server.js to use static ('image') files
-2. Update index.pug and user.pug to display images
-3. Create an "images" folder
-4. Add image files in the images folder
 5. Run start command 
 6. View the page on a web browser
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var app = express()
 var fs = require('fs')
 var path = require('path');
 
+var bodyParser = require('body-parser');
+var _ = require('lodash');
+
 var users = []
 
 fs.readFile(path.resolve(__dirname, './data/users.json'), {encoding: 'utf8'}, function(err, data) {
@@ -15,22 +18,75 @@ fs.readFile(path.resolve(__dirname, './data/users.json'), {encoding: 'utf8'}, fu
   })
 })
 
+function getUserFilePath(username) {
+  return path.join(__dirname, 'data', 'users', username) + '.json';
+}
+
+function getUser(username) {
+  var user = JSON.parse(fs.readFileSync(getUserFilePath(username), { encoding: 'utf8'}))
+  user.name.full = _.startCase(user.name.first + ' ' + user.name.last);
+  _.keys(user.location).forEach(function([key]) {
+    user.location[key] = _.startCase(user.location[key])
+  })
+  return user;
+}
+
+function saveUser (username, data) {
+  var fp = getUserFilePath(username)
+  fs.unlinkSync(fp) // delete the file
+  fs.writeFileSync(fp, JSON.stringify(data, null, 2), {encoding: 'utf8'})
+}
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use('/scripts', express.static(path.join(__dirname, 'public', 'scripts')))
+app.use('/styles', express.static(path.join(__dirname, 'public', 'styles')))
 app.use('/photos', express.static(path.join(__dirname, 'images')))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
   res.render('index', { users: users });
 })
 
-app.get('/:username', function(req, res) {
-  var username = req.params.username;
-  res.render('user', { username: username })
+app.get('/', function (req, res) {
+  var users = []
+  var dir = path.join(__dirname, 'data', 'users');
+  fs.readdir(dir, function (err, files) {
+    files.forEach(function (file) {
+      fs.readFile(path.join(__dirname, 'data', 'users', file), {encoding: 'utf8'}, function (err, data) {
+        var user = JSON.parse(data)
+        user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
+        users.push(user)
+        if (users.length === files.length)  {
+          res.render('index', {users: users})
+        }
+      })
+    })
+  })
 })
 
-app.get('/service', function(req, res) {
-  res.send('This is the service page')
+app.get('/:username', function (req, res) {
+  var username = req.params.username
+  var user = getUser(username)
+  res.render('user', {
+    user: user,
+    address: user.location
+  })
+})
+
+app.put('/:username', function (req, res) {
+  var username = req.params.username
+  var user = getUser(username)
+  user.location = req.body
+  saveUser(username, user)
+  res.end()
+})
+
+app.delete('/:username', function (req, res) {
+  var fp = getUserFilePath(req.params.username)
+  fs.unlinkSync(fp) // delete the file
+  res.sendStatus(200)
 })
 
 var server = app.listen(4000, function() {
