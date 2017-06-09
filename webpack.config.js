f18a3b867927dd98ebf4ceb11ad1cd120498849f
Berlin Sky <pedroyoung8989@gmail.com>
2017-06-08 17:34:27 -0700

    setup

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const autoprefixer = require('autoprefixer');
 
 const javascript = {
   test: /\.(js)$/, 
+  exclude: /node_modules/,
   use: [{
     loader: 'babel-loader',
     options: { presets: ['es2015'] } 
